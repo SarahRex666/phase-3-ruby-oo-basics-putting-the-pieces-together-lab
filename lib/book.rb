8a3373352af7ee60629f1335ebfbdca49d62a53a
SarahRex <sarahrexmakes@gmail.com>
2022-06-13 20:57:10 -0400

    commit tree f35c4ff0d1ed53d0a8bf5a2b9dfe15d601ede1d6

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,9 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author_name
 
-
+    def initialize(title, author_name)
+        @title = title
+        @author_name = author_name
+    end
+end
